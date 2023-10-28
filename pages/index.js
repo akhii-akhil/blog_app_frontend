4e54644fbfcaf2ca5ac2b13434ea54dc432f7fb6
AKHIL GADDAM <akhilgaddam00@gmail.com>
2023-10-29 02:36:54 +0530

    trt

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/pages/Navbar";
 import { createClient } from "next-sanity";
 import { Inter } from "next/font/google";
 import Link from "next/link";
